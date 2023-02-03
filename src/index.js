1cd60c92d6f5ac56a244c5387c9d46aae9fceb7f
Pritam Das <pritamstudy123@gmail.com>
2023-02-03 17:40:51 +0530

    login page

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { DarkModeContextProvider } from "./context/darkModeContext";
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 ReactDOM.render(
   <React.StrictMode>
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,52 @@
-import "./login.scss"
+import { useState } from "react";
+import "./login.scss";
 
 const Login = () => {
+  const [showPassword, setShowPassword] = useState(false);
   return (
-    <div>Login</div>
-  )
-}
+    <section>
+      <div className="box">
+        <div className="square"></div>
+        <div className="square"></div>
+        <div className="square"></div>
+        <div className="square"></div>
+        <div className="square"></div>
+        <div className="square"></div>
 
-export default Login
+        <div className="container">
+          <div className="form">
+            <h2 style={{textAlign:"center"}}>LOGIN</h2>
+            <form action="">
+              <div className="inputBx">
+                <input type="text" required="required" />
+                <span>Login</span>
+                <i className="bi bi-person-circle fas"></i>
+              </div>
+              <div className="inputBx password">
+                <input
+                  id="password-input"
+                  type={showPassword ? "text" : "password"}
+                  name="password"
+                  required="required"
+                />
+                <span>Password</span>
+                <a
+                  href="#"
+                  className={showPassword ? "password-control hide" : "password-control view"}
+                  onClick={() => setShowPassword(!showPassword)}
+                ></a>
+                <i className="bi bi-key-fill fas"></i>
+              </div>
+              <div className="inputBx">
+                <div className="sub-btn">                <input type="submit" value="Log in" disabled /></div>
+
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Login;
